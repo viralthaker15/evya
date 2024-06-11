10066060e75e1e5b123c1bd03954ba01f8a60ba8
Viral Thaker <viralthaker27@gmail.com>
2024-06-12 01:38:27 +0530

    Vercel deployment changes

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,15 @@ import cors from "cors";
 const port = process.env.PORT || 5000;
 const app = express();
 
+async function initializeDatabaseConnection() {
+  const dataSource = await buildDataSource();
+  console.log("Database connections established.");
+
+  return dataSource;
+}
+
+initializeDatabaseConnection();
+
 // Middlewares
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,14 +42,5 @@ const server = app.listen(port, async () => {
   console.log(`Listening on ${port}: ${app.get("env")}`);
   console.log(`  Press CTRL-C to stop\n`);
 });
-
-async function initializeDatabaseConnection() {
-  const dataSource = await buildDataSource();
-  console.log("Database connections established.");
-
-  return dataSource;
-}
-
-initializeDatabaseConnection();
 
 export default app;
